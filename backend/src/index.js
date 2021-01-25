cc417e50f03ba10e3a2b7644a1f4a2432ca3b90f
William <william.cesar@geotech4web.com.br>
2021-01-25 07:39:08 -0300

    Routing configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://william:total90@imagedb.bhfxg.mongodb.net/uploader?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 app.use(require("./routes"));
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,39 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-routes.post("/upload", multer(multerConfig).single("file"), (req, res) => {
-  console.log(req.file);
+const Post = require("./models/post");
 
-  return res.json({
-    Upload: "Success",
-  });
+routes.get("/upload", async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.post(
+  "/upload",
+  multer(multerConfig).single("file"),
+  async (req, res) => {
+    const { originalname: name, size, filename: key, location: url } = req.file;
+
+    const post = await Post.create({
+      name,
+      size,
+      key,
+      url,
+    });
+
+    return res.json({
+      post,
+    });
+  }
+);
+
+routes.delete("/upload/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send("Success!");
 });
 
 module.exports = routes;
